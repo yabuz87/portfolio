6afeb7ff708996d1275926c624a91d36d8334653
Yabets solomon <jabezsolomonz@gmail.com>
2024-11-11 01:41:40 +0300

    font styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import footerLogo from "../../assets/footer_logo.svg"
+import userIcon from "../../assets/user_icon.svg";
 import  "./footer.css";
 
-const footer = () => {
+const Footer = () => {
   return (
     <div className="fotter">
       <div className="footer-top">
@@ -12,13 +13,25 @@ const footer = () => {
     </div>
     <div className="footer-top-left">
       <div className="footer-email-input">
-    
+        <img src={userIcon} alt=""/>
+        <input type="email" placeholder="Enter your email"/>
+
       </div>
+      <div className="footer-sibscribe">Subscribe</div>
     </div>
 
       </div>
+      <hr/>
+      <div className="footer-bottom">
+      <p className="footer-bottom-left">copyright Yabets.All rights </p>
+      <div className="footer-bottom-right">
+        <p>Term of Services</p>
+        <p>Privacy Policy</p>
+        <p>Connect with me</p>
+      </div>
+    </div>
     </div>
   )
 }
 
-export default footer
+export default Footer
