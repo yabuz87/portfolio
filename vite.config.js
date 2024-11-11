bbaaf1d8218ef6224d47ba79f19116eb32a65227
Yabets solomon <jabezsolomonz@gmail.com>
2024-11-11 12:08:16 +0300

    these are to remove the gh-pages branche for the page is not deployed properly

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.JPG'] // Include JPG files in the build
+  base: '/portfolio/', // Your GitHub repository name
+  build: {
+    rollupOptions: {
+      input: './index.html', // Ensure this path is correct
+    },
+  },
+  assetsInclude: ['**/*.JPG'],
 });
-
